f0487fd695a0cbca12de983687980744f20beda5
bjornpetersson1 <bjornpetersson1@live.se>
2026-01-28 20:14:06 +0100

    Structure LevelModel for future levelflow

--- Labb2_DungeonCrawler/State/LevelModel.cs
+++ Labb2_DungeonCrawler/State/LevelModel.cs
@@ -9,9 +9,47 @@ namespace Labb2_DungeonCrawler.State
     public class LevelModel
     {
         public string Name { get; set; }
-        public int AliveEnemies { get; set; }
+        public int InitialEnemies { get; set; }
         public int DeadEnemies { get; set; }
         public List<LevelElement> Elements { get; set; }
         public bool IsAccessable { get; set; }
+
+
+        public void LoadLevelFromFile(string path)
+        {
+            int row = 4;
+            Elements = new List<LevelElement>();
+            foreach (var line in File.ReadAllLines(path))
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case '#':
+                            Elements.Add(new Wall() { yCordinate = row, xCordinate = i });
+                            break;
+                        case '@':
+                            Elements.Add(new Player() { yCordinate = row, xCordinate = i });
+                            break;
+                        case 'r':
+                            Elements.Add(new Rat() { yCordinate = row, xCordinate = i });
+                            InitialEnemies++;
+                            break;
+                        case 's':
+                            Elements.Add(new Snake() { yCordinate = row, xCordinate = i });
+                            InitialEnemies++;
+                            break;
+                        case 'R':
+                            Elements.Add(new TheRatKing() { yCordinate = row, xCordinate = i });
+                            InitialEnemies++;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                row++;
+            }
+        }
     }
 }
+
